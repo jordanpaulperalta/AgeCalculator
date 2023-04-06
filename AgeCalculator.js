6be40ce093b87a86d0fc8901d173ea3f8a1e45a8
jordanpaul <92407629+jordanpaulperalta@users.noreply.github.com>
2023-04-06 16:32:08 -0600

    Update AgeCalculator.js

--- AgeCalculator.js
+++ AgeCalculator.js
@@ -1,19 +1,22 @@
-"use strict";
+'use strict';
 
 const dateObject = new Date();
 const year = dateObject.getFullYear();
+const startBtn = document.getElementById('startBtn');
 
-const userAge = Number(prompt(`Enter your age: `));
+startBtn.addEventListener('click', function () {
+  console.clear();
+  const userAge = Number(prompt(`Enter your age: `));
+  const ageValue = new Number(userAge);
 
-const ageValue = new Number(userAge);
-
-if (userAge == ageValue) {
-  const ageCalc = function (age) {
-    return year - age;
-  };
-  console.log(`The year as of today is ${year}.`);
-  console.log(`You entered your age as ${ageValue}.`);
-  console.log(`You were born in the year ${ageCalc(userAge)}.`);
-} else {
-  console.log("Enter numbers only. Try again!");
-}
+  if (userAge == ageValue) {
+    const ageCalc = function (age) {
+      return year - age;
+    };
+    console.log(`The year as of today is ${year}.`);
+    console.log(`You entered your age as ${ageValue}.`);
+    console.log(`You were born in the year ${ageCalc(userAge)}.`);
+  } else {
+    console.log('Enter numbers only. Try again!');
+  }
+});
